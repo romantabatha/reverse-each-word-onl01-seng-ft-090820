e7646a44ab99a06ef9ecc698a1f505caf718c7d5
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 16:19:00 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+# def reverse_each_word(sentence1)
+#   new_sentence = []
+#   sentence1.split do |sentence1|
+#     new_sentence << sentence1.reverse_each
+#   end
+#   new_sentence
+# end
+
+def reverse_each_word(sentence)
+  sentence.split.collect do |word|
+  word.reverse
+  end.join(' ')
+end
+
+# def reverse_each_word(sentence1)
+#   sentence1.split.collect
+#   new_array.collect do |word|
+#     sentence1.reverse
+#   end
+# end
