3f1c3df6562b389bcd4e65dc5f579fefb51cb0e3
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 15:59:47 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+# def reverse_each_word(sentence1)
+#   new_sentence = []
+#   sentence1.split do |sentence1|
+#     new_sentence << sentence1.reverse_each
+#   end
+#   new_sentence
+# end
+
+# def reverse_each_word(sentence)
+#   sentence.split do |word|
+#   "word".reverse
+#   end
+# end
+
+def reverse_each_word(sentence1)
+  new_array = [sentence1.split]
+  new_array.collect do |word|
+    new_array << sentence1.reverse
+  end
+  new_array.join(" ")
+end
